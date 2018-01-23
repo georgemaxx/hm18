6e4717a3cfc025e342da42826dc6d6b4e9436994
FoxinM <2050897287@qq.com>
2018-01-23 14:04:49 +0800

    test

--- piano/comtest.py
+++ piano/comtest.py
@@ -8,7 +8,7 @@ print (ports)
 
 song1 = ['1','1','5','5','6','6','5','5','4','4','3','3','2','2','1','1']
 song2 = ['1','2','3','1','1','2','3','1','3','4','5','3','4','5']
-
+song3 = ['3','6','7','11','7','6','3','6','7','11','12','11','12','13']
 #f = open('mysongs.csv', 'r')
 #data = f.read()
 #rows = data.split('\n')
@@ -18,7 +18,7 @@ song2 = ['1','2','3','1','1','2','3','1','3','4','5','3','4','5']
 
 for p in ports:
     print (p[1])
-    if "Arduino" in p[1]:
+    if "USB-SERIAL CH340 (COM3)" in p[1]:
 	    ser=serial.Serial(port=p[0])
     else :
 	    print ("No Arduino Device was found connected to the computer")
@@ -29,16 +29,23 @@ for p in ports:
 def run():
     action = "empty"
     while action != "q":
-        print ('select which song do you want to play ? 1,2 q and others for quit')
+        print ("No Arduino Device was found connected to the computer")
         action = input("> ")
-        if action == "1":
+        if action =="1":
             for notes in song1:
                 ser.write(notes.encode())
                 print ("send:"+notes)
                 time.sleep(1)
         elif action == "2":
-            ser.write('2'.encode())
-
+            for notes in song２:
+                ser.write(notes.encode())
+                print ("send:"+notes)
+                time.sleep(1)
+        elif action == "3":
+            for notes in song3:
+                ser.write(notes.encode())
+                print ("send:"+notes)
+                time.sleep(1)
         else :
             return
 
