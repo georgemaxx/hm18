9cad3421e0d2e8209ee214803ee98171335653cd
Lanchaster <Frozentar@qq.com>
2018-01-23 16:47:35 +0800

    Merge branch 'master' of https://github.com/MHM18/hm18

--- hmpro/xuyiheng/work7.py
+++ hmpro/xuyiheng/work7.py
@@ -5,3 +5,11 @@ def sum(arg1,arg2):
 
 total=sum(10,20)
 print("函数外：",total)
+
+def sum1(arg1,arg2,arg3):
+	total=arg1+arg2+arg3
+	print("总分：",total)
+	return total
+
+total=sum1(92,101,97)
+print("总分：",total)
