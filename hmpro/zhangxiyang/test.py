91afc603d4a038194ef884dab7dcf1f36336a946
suisuiyang <30796619+suisuiyang@users.noreply.github.com>
2018-01-23 14:13:57 +0800

    gua

--- hmpro/zhangxiyang/test.py
+++ hmpro/zhangxiyang/test.py
@@ -2,5 +2,11 @@ def hello (ha):
     print("hello",ha)
 hello ("bilibili")
 def area(width,height):
-    print (width*height) 
+    print (width*height)
 area(6,5)
+w = 4
+h = 5
+def myprint(w,h):
+    s = area(w,h)
+    print("width=",w,"height=",h,"area=",a)
+myprint(12,5)
