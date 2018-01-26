bd30d4cd7cede6797c214bc65342951c0f9c601f
suisuiyang <30796619+suisuiyang@users.noreply.github.com>
2018-01-26 11:26:12 +0800

    s

---
+++ hmpro/zhangxiyang/read/read2.py
@@ -0,0 +1,5 @@
+f = open('C:/Users/SuiYa/Documents/GitHub/hm18/hmpro/zhangxiyang/abc.txt','w',encoding='utf-8')
+data = "jhdfskhgskhjgf"
+f.write(data)
+f.close()
+print (data)
---
+++ hmpro/zhangxiyang/read/read3.py
@@ -0,0 +1,5 @@
+f = open('C:/Users/SuiYa/Documents/GitHub/hm18/hmpro/zhangxiyang/abc.txt','a',encoding='utf-8')
+data = r"\nkhgskhjgf"
+f.write(data)
+f.close()
+print (data)
