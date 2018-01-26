6d678942c8d7647793fce977d39d692522d13329
Lanchaster <Frozentar@qq.com>
2018-01-26 10:23:26 +0800

    a

--- hmpro/xuyiheng/a18_1_26/import_from_another_folder/six.py
+++ hmpro/xuyiheng/a18_1_26/import_from_another_folder/six.py
@@ -1,5 +1,5 @@
 #导入模块
-import import
+import sys
 one=imp.load_source('one','C:/Users/Indira/Documents/GitHub/hm18/hmpro/xuyiheng/a18_1_26/import_file_in_the_same_folder/one.py')
 import import_file_in_the_same_folder
 one.printme()
