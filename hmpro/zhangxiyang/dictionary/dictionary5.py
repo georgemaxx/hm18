d531f61e1b43b681e9ed32bfd8de5429922da194
suisuiyang <30796619+suisuiyang@users.noreply.github.com>
2018-01-28 17:21:22 +0800

    ss

---
+++ hmpro/zhangxiyang/dictionary/dictionary5.py
@@ -0,0 +1,12 @@
+a = [66.25,333,333,1,1234.5]
+print(a.count(333),a.count(66.25),a.count('x'))
+a.insert(2,-1)#插入
+a.append(333)
+print(a)
+a.index(333)#索引
+a.remove(333)#删掉第一个333
+print(a)
+a.reverse#倒序排
+print(a)
+a.sort()#从小到大排
+print(a)
