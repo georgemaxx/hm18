bae1abbe8252ad5983487fff11d8c948f714287d
Lanchaster <Frozentar@qq.com>
2018-01-29 09:43:15 +0800

    a

---
+++ hmpro/xuyiheng/a18_1_29/d.py
@@ -0,0 +1,22 @@
+#字符串运算符号
+
+a = 'Hello'
+b = 'Python'
+
+print ("a + b 输出结果： ", a+b)
+print ("a * 2 输出结果： ", a*2)
+print ("a[1]输出结果： ", a[1])
+print("a[1:4]输出结果： ", a[1:4])
+
+if ("H" in a) :
+    print ("H在变量a中")
+else :
+    print ("H不再变量a中")
+
+if ("M" not in a) :
+    print("M不再变量a中")
+else :
+    print("M在变量a中")
+
+print (r'\n')
+print (R'\n')
